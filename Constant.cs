ec64fbb13f8a8931e1458a2a73fcf00846addb42
bub.bl <66354328+bub-bl@users.noreply.github.com>
2021-07-23 15:25:17 +0200

    Reorganizing some codes

--- Constant.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Average
-{
-    internal static class Constant
-    {
-        public const string RESOURCE_NAME = "avg";
-        public const string BASE_PLUGINS_DIRECTORY_NAME = "plugins";
-    }
-}

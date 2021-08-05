ff0e6980243d9ebbb6ca5bd089aaf3fb9c2eea0e
bub.bl <66354328+bub-bl@users.noreply.github.com>
2021-08-05 17:12:15 +0200

    Adding support for Language

---
+++ Managers/LanguageManager.cs
@@ -0,0 +1,24 @@
+﻿using SDK.Client;
+using System.Collections.Generic;
+
+namespace Average.Client.Managers
+{
+    public class LanguageManager
+    {
+        Dictionary<string, string> language = new Dictionary<string, string>();
+        public string Current { get; private set; }
+
+        public LanguageManager() => Load();
+
+        void Load()
+        {
+            var config = Configuration.Parse("config.json");
+            Current = (string)config["Language"];
+            language = Configuration.ParseToDictionary($"languages/{Current}.json");
+        }
+
+        bool KeyExist(string key) => language.ContainsKey(key);
+
+        public string Get(string key) => KeyExist(key) ? language[key] : string.Empty;
+    }
+}
